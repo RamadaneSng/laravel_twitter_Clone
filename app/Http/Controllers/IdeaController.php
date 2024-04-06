3811638779e336951854f0f232e45e76ab64f519
RamadaneSng <iscoramadane@gmail.com>
2024-04-06 15:07:56 +0000

    first commit

---
+++ app/Http/Controllers/IdeaController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Idea;
+use Illuminate\Http\Request;
+
+class IdeaController extends Controller
+{
+  public function store()
+  {
+    request()->validate([
+      "idea" => "required|min:3|max:240",
+    ]);
+
+
+    $idea =  Idea::create([
+      'content' => request()->get('idea')
+    ]);
+
+    return redirect()->route('dashboard')->with('success', 'Idea created successfully ');
+  }
+
+  public function destroy($id)
+  {
+    $idea = Idea::where('id', $id)->firstOrFail();
+
+    $idea->delete();
+
+    return redirect()->route('dashboard')->with('success', 'Idea deleted successfully ');
+  }
+}
